72abefa82b7b5b10dab723f4c081523e9d85382a
Yunus Coskun <yunussemihcoskun@gmail.com>
2021-07-10 19:49:12 -0400

    psets work

--- ProblemSets/PS5.jl
+++ ProblemSets/PS5.jl
@@ -1,12 +1,24 @@
 import Pkg
 
-Pkg.add("Optim")
+# Pkg.add("Optim")
 
-Pkg.add("CSV")
+# Pkg.add("CSV")
 
+# Pkg.add("DataFrames")
+
+# Pkg.add("DelimitedFiles")
+
+using DataFrames
 
 using CSV
-CSV.read("C:\\Users\\cosku\\Desktop\\Xfiles2.csv")
 
-CSV.read("C:\\Users\\cosku\\Desktop\\Yfiles2.csv")
+using DelimitedFiles
+
+X = readdlm("C:/Users/cosku/Desktop/Xfiles2.csv", ';', Float64);
+
+y = readdlm("C:/Users/cosku/Desktop/Yfiles2.csv", ';', Float64);
+
+b0 = 0; b1 = 0; b2 = 0; b3 = 0; b4 = 0; b5 = 0;
+
+Betha = [b0 b1 b2 b3 b4 b5];
 
